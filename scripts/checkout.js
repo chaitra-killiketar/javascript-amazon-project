b73c9763fd6f7bac7483035e968aad2beeaea09b
chaitra-killiketar <killiketarc@gmail.com>
2026-01-14 21:29:35 +0530

    solved problem of checkout quantity

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -103,7 +103,19 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
   container.remove();
+  updatecartQauntity();
   })
 })
 
+updatecartQauntity();
+
+function updatecartQauntity(){
+  let cartQuantity=0;
+
+    cart.forEach((item) => {
+      cartQuantity+=item.quantity;
+    })
+
+    document.querySelector('.js-return-to-home-link').innerHTML=cartQuantity;
+}
 
