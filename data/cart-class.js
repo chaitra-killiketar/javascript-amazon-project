0dbe35657e06d27f7e91ef44ef2487702eeb23b6
chaitra-killiketar <killiketarc@gmail.com>
2026-02-27 20:29:45 +0530

    make locakStorageKey and LoadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,32 +1,32 @@
 class Cart{
-
-  cartItems=undefined;
-  localStoragekey=undefined;
+  cartItems;
+  #localStoragekey;
    
-  constructor(localStoragekey)
-  {
-    this.loadFromStorageKey=localStoragekey;
-    this.loadFromStorage();
-  }
-  loadFromStorage()
-  {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStoragekey));
-
-    if(!this.cartItems){
-      this.cartItems=[{
-      productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity:2,
-      deliveryOptionId:'1'
-    },{
-      productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity:1,
-      deliveryOptionId:'2'
-    }];
+    constructor(localStoragekey)
+    {
+      this.#localStoragekey=localStoragekey;
+      this.#loadFromStorage();
     }
-  }
+    
+    #loadFromStorage()
+    {
+      this.cartItems = JSON.parse(localStorage.getItem(this.#localStoragekey));
+
+      if(!this.cartItems){
+        this.cartItems=[{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2,
+        deliveryOptionId:'1'
+      },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1,
+        deliveryOptionId:'2'
+      }];
+      }
+    }
 
     saveToStorage(){
-      localStorage.setItem(this.localStoragekey,JSON.stringify(this.cartItems));
+      localStorage.setItem(this.#localStoragekey,JSON.stringify(this.cartItems));
     }
 
     addToCart(productId){
@@ -82,7 +82,7 @@ class Cart{
 
 const cart=new Cart('cart-oop');
 const businessCart =new Cart('cart-business');
-// cart.addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+
 console.log(cart);
 console.log(businessCart);
 console.log(businessCart instanceof Cart);
