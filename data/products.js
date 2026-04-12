c5f45a3a54480a996d83b5fa706b69695b02141a
chaitra-killiketar <killiketarc@gmail.com>
2026-04-12 21:10:51 +0530

    final changes

--- data/products.js
+++ data/products.js
@@ -74,6 +74,27 @@ object3.method();
 
 export let products =[];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  return promise;
+}
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -94,7 +115,7 @@ export function loadProducts(fun) {
     console.log('Unexpected error. Please try again later.');
   });
 
-  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.open('GET', 'https://error.supersimplebackend.dev/products');
   xhr.send();
 }
 loadProducts();
